f752024d635bfc38fe303d06cdfce6164426c75c
Jonnathan Sotelo <jhonysotelo.1015@gmail.com>
2023-05-24 20:53:18 -0500

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+// Agregar un event listener al botón
+function miFunc(params) {
+     // Seleccionar el input por su id
+     const user = document.getElementById("user");
+
+     const valor_user = user.value;
+ 
+      const password = document.getElementById("password");
+ 
+      const valor_password = password.value;
+     
+     if ((valor_user == "admin") && (valor_password == "admin")) {
+          window.location.href = './index.html';
+     }else{
+          alert("el usuario o contraseña no son correctos");
+          user.value = null;
+          password.value = null;
+     }
+    
+     
+}
